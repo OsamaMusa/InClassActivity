46d22f8a60c799b04226240d3c75e2e737c284eb
Osama Musa <osama2000musa@gmail.com>
2020-12-03 13:29:16 +0200

    ver 1.0

---
+++ app/src/main/java/com/example/inclassactivity/Car.java
@@ -0,0 +1,15 @@
+package com.example.inclassactivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    String name;
+
+    public Car() {
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
